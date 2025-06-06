4217b755e0a8c969ec008dcd1f0501532ca62de9
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-06 06:30:05 +0530

    fix proteced page

--- netflix_clone/src/App.jsx
+++ netflix_clone/src/App.jsx
@@ -1,27 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import Login from './pages/Login/Login'; 
-import Home from './pages/Home/Home'; 
-import Player from './pages/player/Player'; 
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+import Player from './pages/player/Player';
 import ProtectedRoute from './components/ProtectedRoute';
- 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+
         <Route
-         path="/home"
+          path="/home"
           element={
             <ProtectedRoute>
               <Home />
-              <Route path="/player/:id" element={<Player />} />
             </ProtectedRoute>
           }
         />
-      
-         
+
+        <Route
+          path="/player/:id"
+          element={
+            <ProtectedRoute>
+              <Player />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
